e5100a27ff5b83e30c8b49f825bb631ebdf6616f
abhi114 <nabhi531@gmail.com>
2021-07-09 02:00:31 +0530

    permission functions completed

--- app/src/main/java/com/example/bluetooth_chat/MainActivity.java
+++ app/src/main/java/com/example/bluetooth_chat/MainActivity.java
@@ -1,9 +1,15 @@
 package com.example.bluetooth_chat;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.bluetooth.BluetoothAdapter;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -12,6 +18,7 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
     //to provide functionality like to swithc on/off the bluetooth, list of all the paired devices
     private BluetoothAdapter bluetoothAdapter;
+    private final int LOCATION_PERMISSION_REQUEST = 101;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,6 +63,54 @@ public class MainActivity extends AppCompatActivity {
                 return super.onOptionsItemSelected(item);
         }
 
+    }
+
+    //devices running marsh-mellow and above needs dynamic permission request
+    //Class for retrieving various kinds of information related to the application packages that are currently installed on the device.
+    private void checkPermissions(){
+        //Determine whether you have been granted a particular permission.
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+            //if the permission is not granted
+            //Helper for accessing features in Activity.
+            //Requests permissions to be granted to this application.
+            // String: The requested permissions. Must me non-null and not empty.
+            //int: Application specific request code to match with a result reported to ActivityCompat.OnRequestPermissionsResultCallback.onRequestPermissionsResult
+            // These permissions must be requested in your manifest
+            ActivityCompat.requestPermissions(MainActivity.this,new String[] {Manifest.permission.ACCESS_FINE_LOCATION},LOCATION_PERMISSION_REQUEST);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if(requestCode == LOCATION_PERMISSION_REQUEST){
+            //int: The grant results for the corresponding permissions which is either PackageManager.PERMISSION_GRANTED
+            // or PackageManager.PERMISSION_DENIED. Never null.
+            if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                //if the permission is granted
+
+
+            }else{
+                //if permission is denied
+                new AlertDialog.Builder(this)
+                        .setCancelable(false)
+                        .setMessage("LOCATION PERMISSION IS REQUIRED")
+                        //Interface used to allow the creator of a dialog to run some code when an item on the dialog is clicked.
+                        .setPositiveButton("Grant", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                checkPermissions();
+                            }
+                        }).setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        MainActivity.this.finish();
+                    }
+                }).create();
+            }
+        }else {
+            //if it is some other request
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
     }
 
     //to enable bluetooth
