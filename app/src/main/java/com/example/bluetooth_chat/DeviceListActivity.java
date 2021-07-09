6f70fa127208a19365b6e2ccdf1a0979a73890f0
abhi114 <nabhi531@gmail.com>
2021-07-09 16:56:08 +0530

    available connections

--- app/src/main/java/com/example/bluetooth_chat/DeviceListActivity.java
+++ app/src/main/java/com/example/bluetooth_chat/DeviceListActivity.java
@@ -5,11 +5,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import java.util.Set;
@@ -23,6 +29,8 @@ public class DeviceListActivity extends AppCompatActivity {
 
     //bluetooth adapter can be used to get the list of paired devices
     private BluetoothAdapter bluetoothAdapter;
+
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +47,8 @@ public class DeviceListActivity extends AppCompatActivity {
         adapterPairedDevices = new ArrayAdapter<String>(this,R.layout.device_list_item);
         adapterAvailableDevices = new ArrayAdapter<String>(this,R.layout.device_list_item);
 
+        progressBar = findViewById(R.id.progress_scan_devices);
+
         listPairedDevices.setAdapter(adapterPairedDevices);
         listAvailableDevices.setAdapter(adapterAvailableDevices);
 
@@ -53,7 +63,46 @@ public class DeviceListActivity extends AppCompatActivity {
                 adapterPairedDevices.add(device.getName() + "\n" + device.getAddress());
             }
         }
+        // Register for broadcasts when a device is discovered.
+        //Structured description of Intent values to be matched. An IntentFilter can match against actions,
+        IntentFilter  intentFilter = new IntentFilter(BluetoothDevice.ACTION_FOUND); //Broadcast Action: Remote device discovered.
+        //Register a BroadcastReceiver to be run in the main activity thread.
+        // The receiver will be called with any broadcast Intent that matches filter, in the main application thread.
+        registerReceiver(bluetoothDeviceListener,intentFilter);
+        IntentFilter intentFilter1 = new IntentFilter(BluetoothAdapter.ACTION_DISCOVERY_FINISHED); // Broadcast Action: The local Bluetooth adapter has finished the device discovery process.
+        registerReceiver(bluetoothDeviceListener,intentFilter1);
     }
+    //To receive information about each device discovered,
+    // your app must register a BroadcastReceiver for the ACTION_FOUND intent.
+    //action found -- Broadcast Action: Remote device discovered.
+    //Sent when a remote device is found during discovery.
+    private BroadcastReceiver bluetoothDeviceListener= new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            //Retrieve the general action to be performed, such as ACTION_VIEW.
+            String action = intent.getAction();
+            if(BluetoothDevice.ACTION_FOUND.equals(action)){
+                //The intent contains the extra fields EXTRA_DEVICE and EXTRA_CLASS,
+                // which in turn contain a BluetoothDevice and a BluetoothClass
+                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                //if its not already connected
+                if(device.getBondState() != BluetoothDevice.BOND_BONDED){
+                    adapterAvailableDevices.add(device.getName() + "\n" + device.getAddress());
+                }
+                //Broadcast Action: The local Bluetooth adapter has finished the device discovery process.
+            }else if(BluetoothAdapter.ACTION_DISCOVERY_FINISHED.equals(action)){
+                progressBar.setVisibility(View.GONE);
+                //if no devices has been found
+                if(adapterAvailableDevices.getCount() == 0){
+                    Toast.makeText(DeviceListActivity.this,"No new devices found",Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(DeviceListActivity.this,"click on the device to start the chat",Toast.LENGTH_SHORT).show();
+
+                }
+            }
+
+        }
+    };
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -65,11 +114,31 @@ public class DeviceListActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         switch (item.getItemId()){
             case R.id.menu_scan_devices:
-                Toast.makeText(this,"Scan Devices",Toast.LENGTH_SHORT).show();
+                //Toast.makeText(this,"Scan Devices",Toast.LENGTH_SHORT).show();
+                scanDevices();
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
         }
 
     }
+
+    //scan for available devices
+    private void scanDevices(){
+        progressBar.setVisibility(View.VISIBLE);
+        //we have to hide all the already present available devices and inflate the current devices
+        adapterAvailableDevices.clear();
+
+        Toast.makeText(this,"Scanning",Toast.LENGTH_SHORT).show();
+        //Return true if the local Bluetooth adapter is currently in the device discovery process.
+        if(bluetoothAdapter.isDiscovering()){
+            //Cancel the current device discovery process.
+            bluetoothAdapter.cancelDiscovery();
+        }
+        //The discovery process usually involves an inquiry scan of about 12 seconds,
+        // followed by a page scan of each new device to retrieve its Bluetooth name.
+        bluetoothAdapter.startDiscovery();
+        //Register for BluetoothDevice.ACTION_FOUND to be notified as remote Bluetooth devices are found.
+    }
+
 }
